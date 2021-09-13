4a0966592fe289762edf34c96541358385896ca3
Wan Song <wan.song.rocha@gmail.com>
2021-09-13 11:07:03 -0400

    Delete example_web_page directory

--- example_web_page/server.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const express = require('express');
-const app = express();
-
-app.use(express.static('.'));
-
-app.listen(4000, () => console.log('Example app listening on port 4000'));
